7a9a34f340f9bf56fe57aeee83e804433b34ed33
oliverlai55 <oliverlai55@gmail.com>
2015-12-01 00:53:28 -0500

    Added button and search box

--- bootstrap/js/controller.js
+++ bootstrap/js/controller.js
@@ -3,11 +3,16 @@ var myApp = angular.module('myApp', []);
 myApp.controller('yelpController', function ($scope, $http){
 	$scope.dataList = [];
 
-	var url = "bootstrap/js/clubs.js"
+	$scope.getYelp = function(){
+		var url = "bootstrap/js/clubs.js"
+		$http.get(url).success(function(data){
+			$scope.data = data.businesses
+			console.log($scope.data[8]);
 
-	$http.get(url).success(function(data){
-		$scope.data = data.businesses
-		console.log($scope.data[8]);
+		});
 
-	});
+
+
+	}
+
 });
