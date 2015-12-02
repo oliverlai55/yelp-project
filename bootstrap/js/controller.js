5fa32afe596abeb51c2df78c521ba89876dc63c3
oliverlai55 <oliverlai55@gmail.com>
2015-12-02 09:57:18 -0500

    changes in layout and added youtube videos

--- bootstrap/js/controller.js
+++ bootstrap/js/controller.js
@@ -20,13 +20,21 @@ $(document).ready(function(){
 		
 		TngHtml = '<iframe src="http://snapwidget.com/sc/?h=dG5nYXRsYW50YXxpbnwxNTB8M3wzfHxub3w1fG5vbmV8b25TdGFydHx5ZXN8bm8=&ve=011215" title="Instagram Widget" class="snapwidget-widget" allowTransparency="true" frameborder="0" scrolling="yes" style="border:none; overflow:hidden; width:500px; height:150px"></iframe>'
 		GoldRoomHtml = '<iframe src="http://snapwidget.com/sc/?h=aGF2YW5hYXRsYW50YXxpbnwxNTB8M3wzfHx5ZXN8MjB8bm9uZXxvblN0YXJ0fHllc3xubw==&ve=011215" title="Instagram Widget" class="snapwidget-widget" allowTransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:500px; height:150px"></iframe>'
+		$('#recent-pictures').html('<h3>Recent Pictures and Videos</h3>');
 		$('#club-images').html(TngHtml);
 		$('#club-images2').html(GoldRoomHtml);
+
+		<!-- 4:3 aspect ratio -->
+		
+		videoHtml = '<div class="embed-responsive embed-responsive-4by3"><iframe class="embed-responsive-item" src="https://www.youtube.com/embed/vzis32nUNEo"></iframe></div>';
+		videoHtml2 = '<div class="embed-responsive embed-responsive-4by3"><iframe class="embed-responsive-item" src="https://www.youtube.com/embed/dpw-BDUDxyo"></iframe></div>';
+		$('#club-video').html(videoHtml);
+		$('#club-video2').html(videoHtml2);
+
 	});
 });
 
-		forecastDays += '<div class="title-day col-sm-3">Day</div>';
-		forecastDays += '<div class="title-day2 col-sm-3">2</div>';
+	
 
 	// $scope.clicked = function(){
 	// 	alert("you");
