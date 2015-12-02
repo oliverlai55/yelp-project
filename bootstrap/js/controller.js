58c7fbb80557c27b753364479f8ec7cdf8f48732
oliverlai55 <oliverlai55@gmail.com>
2015-12-02 15:49:24 -0500

    added vine feed

--- bootstrap/js/controller.js
+++ bootstrap/js/controller.js
@@ -24,6 +24,8 @@ $(document).ready(function(){
 		$('#club-images').html(TngHtml);
 		$('#club-images2').html(GoldRoomHtml);
 
+		vineHtml= '<iframe src="https://vine.co/v/O6nArUZunmt/embed/simple" width="500" height="500" frameborder="0"></iframe>';
+		$('#vine-feed').html(vineHtml);
 		<!-- 4:3 aspect ratio -->
 		
 		videoHtml = '<div class="embed-responsive embed-responsive-4by3"><iframe class="embed-responsive-item" src="https://www.youtube.com/embed/vzis32nUNEo"></iframe></div>';
