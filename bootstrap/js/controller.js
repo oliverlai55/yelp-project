8de4eedb828e4edbc9b3d405b5f2203151402b00
Wei lun Lai <oliverlai55@gmail.com>
2015-12-07 09:33:02 -0500

    changing css layout for videos and reviews

--- bootstrap/js/controller.js
+++ bootstrap/js/controller.js
@@ -15,12 +15,15 @@ myApp.controller('yelpController', function ($scope, $http, $sce){
 
 });
 
+//Right Rail Videos and Pictures Updates
 $(document).ready(function(){
 	$("#search-button").click(function(){
 		
-		TngHtml = '<iframe src="http://snapwidget.com/sc/?h=dG5nYXRsYW50YXxpbnwxNTB8M3wzfHxub3w1fG5vbmV8b25TdGFydHx5ZXN8bm8=&ve=011215" title="Instagram Widget" class="snapwidget-widget" allowTransparency="true" frameborder="0" scrolling="yes" style="border:none; overflow:hidden; width:500px; height:150px"></iframe>'
-		GoldRoomHtml = '<iframe src="http://snapwidget.com/sc/?h=aGF2YW5hYXRsYW50YXxpbnwxNTB8M3wzfHx5ZXN8MjB8bm9uZXxvblN0YXJ0fHllc3xubw==&ve=011215" title="Instagram Widget" class="snapwidget-widget" allowTransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:500px; height:150px"></iframe>'
-		$('#recent-pictures').html('<h2>Recent Pictures and Videos</h2>');
+
+
+		TngHtml = '<iframe src="http://snapwidget.com/sc/?h=dG5nYXRsYW50YXxpbnwxNTB8M3wzfHxub3w1fG5vbmV8b25TdGFydHx5ZXN8bm8=&ve=011215" title="Instagram Widget" class="snapwidget-widget" allowTransparency="true" frameborder="0" scrolling="yes" style="border:none; overflow:hidden; width:450px; height:150px"></iframe>'
+		GoldRoomHtml = '<iframe src="http://snapwidget.com/sc/?h=aGF2YW5hYXRsYW50YXxpbnwxNTB8M3wzfHx5ZXN8MjB8bm9uZXxvblN0YXJ0fHllc3xubw==&ve=011215" title="Instagram Widget" class="snapwidget-widget" allowTransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:450px; height:150px"></iframe>'
+		$('#recent-pictures').html("<h2>Recent Pictures and Videos</h2><br><h4>This Week's promotion: Tongue and Groove</h4>");
 		$('#club-images').html(TngHtml);
 		$('#club-images2').html(GoldRoomHtml);
 
